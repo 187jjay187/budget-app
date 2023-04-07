66380146b8f8f7b70104db39bc47018d20cb20cc
187jjay187 <187jjay187@gmail.com>
2023-04-07 14:31:31 +0200

    Create add a category page & show catagories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
-    def icon_tag(icon_name)
-      content_tag(:i, '', class: "fas fa-#{icon_name}")
-    end
+  def icon_tag(icon_name)
+    content_tag(:i, '', class: "fas fa-#{icon_name}")
   end
-  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  devise_scope :user do
+    authenticated :user do
+      root 'groups#index', as: :authenticated_root
+    end
+    
+    unauthenticated do
+      root 'splash_screen#index', as: :unauthenticated_root
+    end
+  end  
+
+  resources :groups, only: [:index, :new, :create, :show, :destroy] do
+    resources :expenses, only: [:index, :new, :create, :show, :destroy]
+  end
 end
